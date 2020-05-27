e6573ed8379631f52d24aab10ca839ef6d6736bd
ayush <ayushrawatmsit@gmail.com>
2020-05-27 12:05:41 +0530

    SignUp and SignIn(without organization)

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,6 +6,8 @@ import MainWindow from './routes/LaunchConf/mainWindow'
 import CreateRoom from './routes/CreateRoom/CreateRoom'
 import JoinRoom from './routes/JoinRoom/JoinRoom'
 import ScheduleMeeting from './routes/Schedule/ScheduleMeeting'
+import SignUp from './routes/SignUpPage/SignUp'
+import SignIn from './routes/SignInPage/SignIn'
 
 import {Route,Switch} from 'react-router-dom'
 
@@ -38,6 +40,8 @@ class App extends Component {
       <Switch>
           {/* <Route path ='/' exact component={Root}/> */}
           <Route path ='/' exact component={Merged}/>
+          <Route path ='/signin' exact component={SignIn}/>
+          <Route path ='/signup' exact component={SignUp}/>
           <Route path ='/launchconf'  component={MainWindow}/>
           <Route path ='/createroom' exact component={CreateRoom}/>
           <Route path ='/joinroom' exact component={JoinRoom}/>
